407989759b26bc74a68c8d37f167ae4df28d1fc0
ishita-saxena10 <sishita468@gmail.com>
2022-11-11 22:09:12 +0530

    docker imgg

--- unit_test.py
+++ unit_test.py
@@ -30,25 +30,23 @@ class TestCalc(unittest.TestCase):
 
 
     def test_add(self):
-        self.assertEqual(add(5,10), 15)
-        self.assertEqual(add(-2,2), 0)
-        self.assertEqual(add(-1,-5), -6)
+        self.assertEqual(add(2,10), 12)
+        self.assertEqual(add(-4,4), 0)
 
 
     def test_substract(self):
-        self.assertEqual(subtract(5,-10), 15)
-        self.assertEqual(subtract(-2,-2), 0)
-        self.assertEqual(subtract(1,5), -4)
+        self.assertEqual(subtract(2,-10), 12)
+        self.assertEqual(subtract(-4,-4), 0)
 
 
     def test_multiply(self):
-        self.assertEqual(product(5,-9.0), -45)
-        self.assertEqual(product(-2,0), 0)
+        self.assertEqual(product(5,-9), -45)
+        self.assertEqual(product(4,0), 0)
         self.assertEqual(product(-10,-5), 50)
 
 
     def test_divide(self):
-        self.assertEqual(divide(5,2), 2.5)
+        self.assertEqual(divide(10,2), 5)
         self.assertRaises(ValueError, divide, 10, 0)
         
 
